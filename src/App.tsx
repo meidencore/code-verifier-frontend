cac66935f08c9855be20b838c81bf4805e69770d
Meidencore <danieleverdale@gmail.com>
2024-04-20 10:11:41 -0400

    refac of the routes to a Routes component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // React Router DOM Imports
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import AppRoutes from './routes/Routes'
+import CopyRights from './components/dashboard/CopyRights'
 
-import { HomePage, KatasDetailPage, KatasPage, LoginPage, RegisterPage } from './pages'
 function App() {
 
   return (
@@ -22,24 +23,10 @@ function App() {
               <Link to='/katas'>Katas</Link>
             </li>
           </ul>
-        </nav>
-        {/* TODO Export to routes folder */}
-        <Routes>
-          {/* Routes definition */}
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/login' element={<LoginPage />}></Route>
-          <Route path='/register' element={<RegisterPage />}></Route>
-          <Route path='/katas' element={<KatasPage />}></Route>
-          <Route path='/katas/:id' element={<KatasDetailPage />}></Route>
-          {/* Redirect when the Page is Not Found */}
-          <Route
-          path='*'
-          element={<Navigate to='/' replace/>}
-          >
-
-          </Route>
-        </Routes>
+        </nav>   
+        <AppRoutes/>   
       </Router>
+      <CopyRights />
     </>
   )
 }
